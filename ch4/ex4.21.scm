016e73b4256f5f2b491564f596379f432ec78e31
Kouhei Suzuki <kouhei.szk@gmail.com>
2014-03-17 14:27:43 +0900

    4.21

--- ch4/ex4.21.scm
+++ ch4/ex4.21.scm
@@ -45,40 +45,60 @@
 
 ((lambda (fact)
    (fact fact 10))
- (lambda (ft k)
+ (lambda (ft k) ;; 手続きft と 引数k をとる
    (if (= k 1)
        1
        (* k (ft ft (- k 1))))))
 
+
+;; fibを作る
+
+(define (fib n)
+  (let fib-iter ((k n))
+    (cond ((= k 0) 0)
+          ((= k 1) 1)
+          (else (+ (fib-iter (- k 1))
+                   (fib-iter (- k 2)))))))
+(fib 10)
+
 ;; ↓
 
-((lambda (ft k)
-   (if (= k 1) 
-       1
-       (* k (ft ft (- k 1)))))
- (lambda (ft k)
-   (if (= k 1)
-       1
-       (* k (ft ft (- k 1)))))
+((lambda (n)
+   (let fib-iter ((k n))
+     (cond ((= k 0) 0)
+           ((= k 1) 1)
+           (else (+ (fib-iter (- k 1))
+                    (fib-iter (- k 2)))))))
  10)
 
 ;; ↓
 
-(if (= 10 1)
-    1
-    (* 10 ((lambda (ft k)
-             (if (= k 1)
-                 1
-                 (* k (ft ft (- k 1)))))
-           (lambda (ft k)
-             (if (= k 1)
-                 1
-                 (* k (ft ft (- k 1)))))
-           9)))
-
-
+((lambda (n)
+   ((lambda (fib)
+      (fib fib n))
+    (lambda (ft k)
+      (cond ((= k 0) 0)
+            ((= k 1) 1)
+            (else (+ (ft ft (- k 1))
+                     (ft ft (- k 2))))))))
+ 10)
 
 ;; b
 
+; (define (f x)
+;   ((lambda (even? odd?)
+;      (even? even? odd? x))
+;    (lambda (ev? od? n)
+;      (if (= n 0) true (od? ⟨??⟩ ⟨??⟩ ⟨??⟩)))
+;    (lambda (ev? od? n)
+;      (if (= n 0) false (ev? ⟨??⟩ ⟨??⟩ ⟨??⟩)))))
+
+(define (f x)
+  ((lambda (even? odd?)
+     (even? even? odd? x))
+   (lambda (ev? od? n)
+     (if (= n 0) true (od? ev? od? (- n 1))))
+   (lambda (ev? od? n)
+     (if (= n 0) false (ev? ev? od? (- n 1))))))
 
 
