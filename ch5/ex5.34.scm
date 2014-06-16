65d06969715fd82e07075242a6a20909c43a0288
Kouhei SUZUKI <kohei.suzuki@mixi.co.jp>
2014-06-16 15:10:58 +0900

    ex5.34

--- ch5/ex5.34.scm
+++ ch5/ex5.34.scm
@@ -19,16 +19,18 @@
  (val) 
  ((assign val (op make-compiled-procedure) (label entry45) (reg env)) 
   (goto (label after-lambda46)) 
-entry45 ; factorialの呼出しスタート
+entry45
   (assign env (op compiled-procedure-env) (reg proc))
   (assign env (op extend-environment) (const (n)) (reg argl) (reg env)) 
   (assign val (op make-compiled-procedure) (label entry47) (reg env)) 
   (goto (label after-lambda48)) 
-entry47 
+entry47 ; factorialの呼出しスタート
   (assign env (op compiled-procedure-env) (reg proc)) 
-  (assign env (op extend-environment) (const (product counter)) (reg argl) (reg env)) 
+  (assign env (op extend-environment) (const (product counter)) (reg argl) (reg env))
+;; 手続き本体の開始
   (save continue) 
-  (save env) 
+  (save env)
+;; (> counter n)
   (assign proc (op lookup-variable-value) (const >) (reg env)) 
   (assign val (op lookup-variable-value) (const n) (reg env)) 
   (assign argl (op list) (reg val))
@@ -43,55 +45,70 @@ compiled-branch53
 primitive-branch52 
   (assign val (op apply-primitive-procedure) (reg proc) (reg argl)) 
 after-call54 
-  (restore env) 
-  (restore continue) 
+  (restore env) ; x
+  (restore continue) ; x
   (test (op false?) (reg val)) 
   (branch (label false-branch50)) 
-true-branch49 
+true-branch49
+;; 反復の終了条件を満たした時
   (assign val (op lookup-variable-value) (const product) (reg env)) 
   (goto (reg continue))
-  false-branch50 
+false-branch50
+;; 反復
+;; (iter (* counter product) (+ counter 1))
   (assign proc (op lookup-variable-value) (const iter) (reg env)) 
-  (save continue) 
+  (save continue)
+;; iterを退避
   (save proc) 
-  (save env) 
+  (save env)
+;; (+ counter 1)
   (assign proc (op lookup-variable-value) (const +) (reg env)) 
   (assign val (const 1)) 
   (assign argl (op list) (reg val)) 
   (assign val (op lookup-variable-value) (const counter) (reg env)) 
   (assign argl (op cons) (reg val) (reg argl)) 
-  (test (op primitive-procedure?) (reg proc)) 
+  (test (op primitive-procedure?) (reg proc)) ; procは+
   (branch (label primitive-branch58)) 
 compiled-branch59 
   (assign continue (label after-call60)) 
   (assign val (op compiled-procedure-entry) (reg proc)) 
   (goto (reg val)) 
-primitive-branch58 
+primitive-branch58
+;; (+ counter 1)を計算
   (assign val (op apply-primitive-procedure) (reg proc) (reg argl)) 
-after-call60 
+after-call60
+;; 結果をarglにリストで保存
   (assign argl (op list) (reg val)) 
-  (restore env) 
-  (save argl) 
+  (restore env)
+;; arglを退避
+  (save argl)
+;; (* counter product)
   (assign proc (op lookup-variable-value) (const *) (reg env)) 
   (assign val (op lookup-variable-value) (const product) (reg env)) 
   (assign argl (op list) (reg val)) 
   (assign val (op lookup-variable-value) (const counter) (reg env)) 
   (assign argl (op cons) (reg val) (reg argl)) 
-  (test (op primitive-procedure?) (reg proc)) 
+  (test (op primitive-procedure?) (reg proc)) ; procは* 
   (branch (label primitive-branch55)) 
 compiled-branch56 
   (assign continue (label after-call57)) 
   (assign val (op compiled-procedure-entry) (reg proc)) 
-  (goto (reg val)) primitive-branch55 
+  (goto (reg val)) 
+primitive-branch55
+;; (* counter product)を計算
   (assign val (op apply-primitive-procedure) (reg proc) (reg argl)) 
-after-call57 
-  (restore argl) 
-  (assign argl (op cons) (reg val) (reg argl)) 
+after-call57
+;; argl、つまり(+ counter 1)の結果の入ったリストを復帰
+  (restore argl)
+;; valには(* counter product)の結果が入っているので、consでiterに実際に渡す引数の列をつくる
+  (assign argl (op cons) (reg val) (reg argl))
+;; iterを復帰する
   (restore proc) 
   (restore continue) 
   (test (op primitive-procedure?) (reg proc)) 
   (branch (label primitive-branch61)) 
-compiled-branch62 
+compiled-branch62
+;; 合成手続きは末尾再帰的に呼び出される
   (assign val (op compiled-procedure-entry) (reg proc))
   (goto (reg val))
 primitive-branch61 
@@ -104,7 +121,7 @@ after-lambda48
   (perform (op define-variable!) (const iter) (reg val) (reg env)) 
   (assign val (const ok)) 
   (assign proc (op lookup-variable-value) (const iter) (reg env))
-  ;; (iter 1 1) のiterに渡す引数部分を作ってる
+;; (iter 1 1) のiterに渡す引数部分を作ってる
   (assign val (const 1)) 
   (assign argl (op list) (reg val)) 
   (assign val (const 1)) 
@@ -112,6 +129,7 @@ after-lambda48
   (test (op primitive-procedure?) (reg proc)) 
   (branch (label primitive-branch64)) 
 compiled-branch65 
+;; 合成手続きは末尾再帰的に呼び出される
   (assign val (op compiled-procedure-entry) (reg proc)) 
   (goto (reg val)) 
 primitive-branch64 
@@ -121,3 +139,10 @@ after-call66
 after-lambda46 
   (perform (op define-variable!) (const factorial) (reg val) (reg env)) 
   (assign val (const ok))))
+
+
+;; 末尾再帰になっている
+;; 合成手続きで末尾再帰的に呼び出されている
+;; スタックに計算途中の値を退避してまた復帰する必要が無い点が異なる
+
+
